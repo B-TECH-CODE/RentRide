92eb31ded24c02d23aa141d4816fbfc53d01df6d
git remote add origin https://github.com/B-TECH-CODE/developer01.git <boinarageswararao@gmail.com>
2026-09-04 17:48:36 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://rentride-neas.onrender.com/api",
 });
 
 export const getCars = async () => {
